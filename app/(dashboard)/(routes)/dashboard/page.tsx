6431bd55c2504268f76ba93682d61e6b80974e9b
Herbert Ntim <herbertntim15@gmail.com>
2023-11-23 21:23:17 +0000

    working on dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,15 @@
 const Dashboard = () => {
   return (
-    <>
-      <p>Dashboard</p>
-    </>
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl text-center font-bold">
+          Explore the power of AI
+        </h2>
+        <p className="text-center text-muted-foreground font-light text-sm md:text-lg">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+    </div>
   )
 }
 
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,15 +1,68 @@
+'use client'
+
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
 import { cn } from "@/lib/utils"
 import { Montserrat } from "next/font/google"
 import Image from "next/image"
 import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+
+export const routes = [
+  {
+    label: 'Dashboard',
+    icon: LayoutDashboard,
+    href: '/dashboard',
+    color: "text-sky-500"
+  },
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    href: '/conversation',
+    color: "text-violet-500",
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: "text-pink-700",
+    href: '/image',
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: "text-orange-700",
+    href: '/video',
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    color: "text-emerald-500",
+    href: '/music',
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    color: "text-green-700",
+    href: '/code',
+  },
+  {
+    label: 'Settings',
+    icon: Settings,
+    color: 'text-blue-900',
+    href: '/settings',
+  },
+];
+
 
 // adding a custom font 
 const montserrat = Montserrat({
-  weight: "800",
+  weight: "600",
   subsets: ['latin']
 })
 
 const Sidebar = () => {
+  const pathname = usePathname()
+
   return (
     <div className="space-y-4 h-full bg-[#111827] text-white flex flex-col py-4">
       <div className="px-3 py-2 flex-1">
@@ -23,6 +76,24 @@ const Sidebar = () => {
           </div>
           <h1 className={cn ("text-2xl font-bold", montserrat.className)}>Creator</h1>
         </Link>
+
+        {/* using routes from the constants folder */}
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              key={route.href} 
+              href={route.href}
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/20" : "text-zinc-400"
+              )}
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   )
